88202195beecccb6ea453fffdc03a0958f0e1d12
Lemuel Zara <lemuel.czara@gmail.com>
2021-05-27 21:15:40 -0300

    feat: ensure createCurrency throw if repository throw

--- src/currencies/currencies.service.ts
+++ src/currencies/currencies.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@nestjs/common';
 
+export class Currencies {
+  currency: string;
+  value: number;
+}
+
 export class CurrenciesRepository {
-  async getCurrency(currency: string): Promise<any> {}
+  async getCurrency(currency: string): Promise<Currencies> {
+    return new Currencies();
+  }
+
+  async createCurrency({ currency, value }): Promise<Currencies> {
+    return new Currencies();
+  }
 }
 
 @Injectable()
 export class CurrenciesService {
   constructor(private currenciesRepository: CurrenciesRepository) {}
 
-  async getCurrency(currency: string): Promise<any> {
+  async getCurrency(currency: string): Promise<Currencies> {
     return await this.currenciesRepository.getCurrency(currency);
+  }
+
+  async createCurrency({ currency, value }): Promise<Currencies> {
+    return await this.currenciesRepository.createCurrency({ currency, value });
   }
 }
