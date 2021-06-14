73e695ccde010e5929f9022133a1e3856512f192
lemuelZara <lemuel.czara@gmail.com>
2021-06-14 16:23:13 -0300

    feat: create currencies input type

--- src/currencies/currencies.service.ts
+++ src/currencies/currencies.service.ts
@@ -1,6 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { Currencies } from './currencies.entity';
 import { CurrenciesRepository } from './currencies.repository';
+import { CurrenciesInputType } from './types/currency-input.type';
 
 @Injectable()
 export class CurrenciesService {
@@ -10,7 +11,10 @@ export class CurrenciesService {
     return await this.currenciesRepository.getCurrency(currency);
   }
 
-  async createCurrency({ currency, value }): Promise<Currencies> {
+  async createCurrency({
+    currency,
+    value,
+  }: CurrenciesInputType): Promise<Currencies> {
     if (value <= 0) {
       throw new BadRequestException('The value must be greater zero.');
     }
@@ -18,7 +22,10 @@ export class CurrenciesService {
     return await this.currenciesRepository.createCurrency({ currency, value });
   }
 
-  async updateCurrency({ currency, value }): Promise<Currencies> {
+  async updateCurrency({
+    currency,
+    value,
+  }: CurrenciesInputType): Promise<Currencies> {
     if (value <= 0) {
       throw new BadRequestException('The value must be greater zero.');
     }
