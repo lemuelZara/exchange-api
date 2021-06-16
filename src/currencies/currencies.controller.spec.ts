e8b0bb6c752a8fcfd69ce40a87dd39206e073bfc
lemuelZara <lemuel.czara@gmail.com>
2021-06-16 14:12:32 -0300

    refactor: change mock error

--- src/currencies/currencies.controller.spec.ts
+++ src/currencies/currencies.controller.spec.ts
@@ -1,15 +1,29 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CurrenciesController } from './currencies.controller';
+import { CurrenciesService } from './currencies.service';
 
 describe('CurrenciesController', () => {
   let controller: CurrenciesController;
+  let service: CurrenciesService;
 
   beforeEach(async () => {
+    const mockCurrenciesService = {
+      getCurrency: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CurrenciesController],
+      providers: [
+        {
+          provide: CurrenciesService,
+          useFactory: () => mockCurrenciesService,
+        },
+      ],
     }).compile();
 
     controller = module.get<CurrenciesController>(CurrenciesController);
+    service = module.get<CurrenciesService>(CurrenciesService);
   });
 
   it('should be defined', () => {
@@ -18,7 +32,13 @@ describe('CurrenciesController', () => {
 
   describe('getCurrency', () => {
     it('should be throw when service throws', async () => {
-      expect(controller.getCurrency).rejects.toThrow();
+      jest
+        .spyOn(service, 'getCurrency')
+        .mockRejectedValue(new BadRequestException());
+
+      expect(controller.getCurrency('INVALID')).rejects.toThrow(
+        new BadRequestException(),
+      );
     });
   });
 });
--- src/currencies/currencies.controller.ts
+++ src/currencies/currencies.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Currencies } from './currencies.entity';
+import { CurrenciesService } from './currencies.service';
 
 @Controller('currencies')
 export class CurrenciesController {
-	@Get()
-	async getCurrency() {
-		throw new Error();
-	}
+  constructor(private currenciesService: CurrenciesService) {}
+
+  @Get('/:currency')
+  async getCurrency(@Param('currency') currency: string): Promise<Currencies> {
+    return await this.currenciesService.getCurrency(currency);
+  }
 }
