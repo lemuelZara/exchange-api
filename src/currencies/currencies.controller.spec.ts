36b336111c74184615975304c99dbe70dc52b34e
lemuelZara <lemuel.czara@gmail.com>
2021-06-17 11:06:52 -0300

    test: ensure createCurrency throw when service throws

--- src/currencies/currencies.controller.spec.ts
+++ src/currencies/currencies.controller.spec.ts
@@ -7,10 +7,12 @@ import { CurrenciesService } from './currencies.service';
 describe('CurrenciesController', () => {
   let controller: CurrenciesController;
   let service: CurrenciesService;
+  let mockData;
 
   beforeEach(async () => {
     const mockCurrenciesService = {
       getCurrency: jest.fn(),
+      createCurrency: jest.fn(),
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -25,6 +27,10 @@ describe('CurrenciesController', () => {
 
     controller = module.get<CurrenciesController>(CurrenciesController);
     service = module.get<CurrenciesService>(CurrenciesService);
+    mockData = {
+      currency: 'USD',
+      value: 1,
+    } as Currencies;
   });
 
   it('should be defined', () => {
@@ -49,14 +55,21 @@ describe('CurrenciesController', () => {
     });
 
     it('should be returns success service data', async () => {
-      const mockData = {
-        currency: 'USD',
-        value: 1,
-      } as Currencies;
-
       jest.spyOn(service, 'getCurrency').mockResolvedValue(mockData);
 
       expect(await service.getCurrency('USD')).toEqual(mockData);
+    });
+  });
+
+  describe('CreateCurrency', () => {
+    it('should be throw when service throws', async () => {
+      jest
+        .spyOn(service, 'createCurrency')
+        .mockRejectedValue(new BadRequestException());
+
+      expect(controller.createCurrency(mockData)).rejects.toThrow(
+        new BadRequestException(),
+      );
     });
   });
 });
