b73e26e56210269adef8fd26541dba84381b0731
lemuelZara <lemuel.czara@gmail.com>
2021-06-18 14:19:41 -0300

    feat: ensure convertAmount calls service with correct params

--- src/exchange/exchange.controller.ts
+++ src/exchange/exchange.controller.ts
@@ -1,10 +1,13 @@
 import { BadRequestException, Controller, Get } from '@nestjs/common';
+import { ExchangeService } from './exchange.service';
 import { ExchangeInputType } from './types/exchange-input.type';
 
 @Controller('exchange')
 export class ExchangeController {
+  constructor(private exchangeService: ExchangeService) {}
+
   @Get('/')
   async convertAmount(data: ExchangeInputType) {
-    throw new BadRequestException();
+    return this.exchangeService.convertAmount(data);
   }
 }
